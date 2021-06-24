072bfa99506122483f1b55e37060dc4e4757d969
Afonne-CID <delightinbusiness@gmail.com>
2021-06-24 13:32:04 -0700

    lagest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,22 +1,26 @@
 #include "holberton.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
+* largest_number - returns the largest of 3 numbers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
 
 int largest_number(int a, int b, int c)
 {
-	int largest = b;
+	int largest;
 
-	if (a > b)
+	if ((a > b) && (b > c))
 	{
 		largest = a;
 	}
-	else if (c > b)
+	else if ((b > a) && (a > c))
+	{
+		largest = b;
+	}
+	else
 	{
 		largest = c;
 	}
