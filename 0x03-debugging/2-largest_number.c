db6af8e1d9ddfc54e7e5255611c01a892efc9b7c
Afonne-CID <delightinbusiness@gmail.com>
2021-06-24 13:53:00 -0700

    lagest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,6 +12,22 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
+	if ((a < 0) || (b < 0) || (c < 0))
+	  {
+	    if (a < 0)
+	      {
+		a = a * -1;
+	      }
+	    else if (b < 0)
+	      {
+		b = b * -1;
+	      }
+	    else
+	      {
+		c = c * -1;
+	      }
+	  }
+
 	if ((a > b) && (b > c))
 	{
 		largest = a;
