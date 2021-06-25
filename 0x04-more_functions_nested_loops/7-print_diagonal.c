d301e9bc4c7e1bad8d9bda70cbd3a4df3e095552
Afonne-CID <delightinbusiness@gmail.com>
2021-06-25 06:24:33 -0700

    working?

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,15 +13,13 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (count = 0; count < n; count++)
+		for (count = 1; count < n; count++)
 		{
-			_putchar(' ');
+			_putchar(32);
 		}
-		_putchar('\\');
+		_putchar(92);
 		_putchar('\n');
 	}
-	else
-	{
-		_putchar('\n');
-	}
+
+	_putchar('\n');
 }
