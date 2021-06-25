c5a3fdc9d8a3ec2bce068735e98a4a1b1311b5d4
Afonne-CID <delightinbusiness@gmail.com>
2021-06-25 12:29:46 -0700

    message

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 	char mult5[] = "Buzx";
 	int count;
 
-	for (count = 1; count <= 100; count++)
+	for (count = 0; count < 100; count++)
 	{
 		if (count % 3 == 0 && count % 5 != 0)
 		{
