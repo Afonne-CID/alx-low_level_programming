b166cc8e4bcfdaa774898dd87a441a681fb74b70
Afonne-CID <delightinbusiness@gmail.com>
2021-06-29 13:38:55 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,26 +1,23 @@
 #include "holberton.h"
+#include <string.h>
 
 /**
- * puts_half - function that prints half of a string
+ * _strcpy - Function copies the string in the copied source
  *
- * @str: pointer to char
- * Return: nothing
+ * @dest: Destination
+ * @src: Source
+ *
+ * Return: dest
  */
-
-void puts_half(char *str)
+char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int n = 0;
+	int i;
 
-	while (str[i] != '\0')
-		i += 1;
-	n = i / 2;
-	if (i % 2 == 1)
-		n += 1;
-	while (str[n] != '\0')
+	for (i = 0; src[i] <= '\0'; i++)
 	{
-		_putchar(*(str + n));
-		n++;
+		dest[i] = src[i];
+		i++;
 	}
-	_putchar('\n');
+
+	return (dest);
 }
