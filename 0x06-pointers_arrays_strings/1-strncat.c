d15e5cfa3501910a412fcd946147965533f47c6a
Afonne-CID <delightinbusiness@gmail.com>
2021-06-30 05:01:42 -0700

    commited

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * _strncat - Function concatenates a specified number from source to destination
+ *
+ * @dest - destination string
+ * @src - source string
+ *
+ * Return: pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, count, copy;
+
+	if (n > 0)
+	{
+
+		for (i = 0; *(src + i) != '\0' && i <= n;)
+		{
+			i++;
+		}	
+
+		for (count = 0; *(dest + count) != '\0';)
+		{
+			count++;
+		}
+
+		copy = 0;
+
+		while (copy < count)
+		{
+			dest[i] = src[copy];
+			i++;
+			copy++;
+		}
+	}
+
+	return (dest);
+}
