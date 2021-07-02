732f227ac4c2d57d55e8e89e6d1955a2c0dcbb3e
Afonne-CID <delightinbusiness@gmail.com>
2021-07-02 05:34:50 -0700

    committed:

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 {
 	int i;
 	int cnt;
-	char separators[] = {32, 10, 9, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	char separators[] = {48, 32, 10, 9, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
