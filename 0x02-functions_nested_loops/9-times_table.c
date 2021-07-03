61d0a2c29285dfcea269d090245fd9f7c8cf5f81
Afonne-CID <delightinbusiness@gmail.com>
2021-07-03 16:00:59 -0700

    edited

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,15 +13,7 @@ void times_table(void)
 		for (cnt = 0; cnt <= times; cnt++)
 		{
 			multi = i * cnt;
-<<<<<<< HEAD:9-times_table.c
 			if (multi >= 10)
-=======
-<<<<<<< HEAD:9-times_table.c
-			if (multi >= 10)
-=======
-			if (multi > 10)
->>>>>>> d96f23f8f50f6a6e147b6ac3f5f29513ff66c0e2:0x02-functions_nested_loops/9-times_table.c
->>>>>>> 34110d833872e1d0cb66f505c93b9ff934528459:0x02-functions_nested_loops/9-times_table.c
 			{
 				_putchar(multi / 10 + '0');
 				_putchar(multi % 10 + '0');
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * main - check the code for ALX School students.
@@ -8,9 +7,6 @@
  */
 int main(void)
 {
-    int n;
-
-    n = add(89, 9);
-    printf("%d\n", n);
+    times_table();
     return (0);
 }
