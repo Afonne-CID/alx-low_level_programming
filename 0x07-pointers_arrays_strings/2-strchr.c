3291b61357a3a41fd96ab7788575db75678c880d
Afonne-CID <delightinbusiness@gmail.com>
2021-07-05 12:50:52 -0700

    find c and return remaing part of the string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function finds and returns a character in a string
+ * @s: first parameter (containing string to find char in)
+ * @c: second parameter (the string to be found in string)
+ *
+ * Return: character
+ */
+char *_strchr(char *s, char c)
+{
+	int cnt;
+
+	for (cnt = 0; s[cnt] != '\0'; cnt++)
+	{
+		if (s[cnt] == c)
+		{
+			return (&s[cnt]);
+		}
+	}
+
+	return ('\0');
+}
