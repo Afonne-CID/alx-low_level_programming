cdafcf6394927e92c680ab63404499cca6b714e7
Afonne-CID <delightinbusiness@gmail.com>
2021-07-05 09:47:24 -0700

    edited

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,6 +5,8 @@
  * @s: parameter 1 (pointer to character) - the memory address to be filled
  * @b: parameter 2 (pointer to character) - the character to fill
  * @n: parameter 3 (pointer to integer) - the count of bits to be filled
+ *
+ * Return: Result
  */
 char *_memset(char *s, char b, unsigned int n)
 {
