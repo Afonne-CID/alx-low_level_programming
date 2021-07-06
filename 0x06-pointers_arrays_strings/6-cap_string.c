797b19e555667819c2aa04d1d9a5057fb1a6305d
Afonne-CID <delightinbusiness@gmail.com>
2021-07-06 01:18:32 -0700

    edited

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,6 +20,13 @@ char *cap_string(char *str)
 			{
 				str[i + 1] -= 32;
 			}
+			else
+			{
+				if (str[i] >= 'A' && str[i] <= 'Z')
+				{
+					str[i] += 32;
+				}
+			}
 		}
 	}
 	return (str);
