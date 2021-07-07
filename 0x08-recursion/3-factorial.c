4406200acb6703526724cc829c66749a9d67f51f
Afonne-CID <delightinbusiness@gmail.com>
2021-07-07 07:26:00 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * factorial - returns the factorial of a give number
+ * @n: parameter containing the factorial
+ *
+ * Return: factorial of the number
+ */
+int factorial(int n)
+{
+	if (i <= 0)
+		return (1);
+	return (factorial(n * (n - 1)));
+}
