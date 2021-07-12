ed5b08c424d20846bae6533651a95d0545e111fc
Afonne-CID <delightinbusiness@gmail.com>
2021-07-12 08:41:21 -0700

    cents

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - entry point
+ * @argc: pramater for keeping count of params
+ * @argv: paramter (pointer to array of pointers)
+ *
+ * Return: Always 0, success
+ */
+int main(int argc, char *argv[])
+{
+	
+}
