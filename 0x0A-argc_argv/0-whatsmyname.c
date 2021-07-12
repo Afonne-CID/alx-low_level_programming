48af4b2eb4398ba089238e4b3da6baceab050149
Afonne-CID <delightinbusiness@gmail.com>
2021-07-12 01:15:14 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: parameter one (argument count)
+ * @argv: parameter two (argument vector)
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
