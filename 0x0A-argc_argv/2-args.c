e6d1ed9f7cc960ab06f1517e1ed9a2dd292b4335
Afonne-CID <delightinbusiness@gmail.com>
2021-07-12 02:10:23 -0700

    using args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: parameter 1
+ * @argv: parmater 2
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int cnt;
+
+	for (cnt = 0; cnt < argc; cnt++)
+	{
+		printf("%s\n", argv[cnt]);
+	}
+
+	return (0);
+}
