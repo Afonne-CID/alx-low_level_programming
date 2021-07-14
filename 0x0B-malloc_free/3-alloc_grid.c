b73252f04859d9eee46f0ed1c704b3b13f02db74
Afonne-CID <delightinbusiness@gmail.com>
2021-07-14 13:57:22 -0700

    commite

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j;
+	int i, j, z;
 	int **grid;
 
 	if (width <= 0 || height <= 0)
@@ -33,5 +33,4 @@ int **alloc_grid(int width, int height)
 		}
 	}
 	return (grid);
-	free(grid);
 }
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * free_grid - func frees dynamic memory
+ * @grid: param 1
+ * @height: param 2
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i, j;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+}
