152423a085a64f381fd89853c22e66e2c381ed65
Afonne-CID <delightinbusiness@gmail.com>
2021-07-14 08:52:06 -0700

    fixes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,30 +18,20 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	for (i = 0; *(s1 + i) != '\0';)
-	{
 		i++;
-	}
-
 	for (j = 0; *(s2 + j) != '\0'; j++)
-	{
 		i++;
-	}
 	i++;
-
-	concat = malloc((sizeof(char) * i) + 1);
-	
+	concat = malloc(sizeof(char) * i);
 	if (concat == NULL)
 		return (NULL);
-	
 	count = 0;
 	while (*(s1 + count) != '\0')
 	{
 		concat[count] = *(s1 + count);
 		count++;
 	}
-
 	z = 0;
 	while (*(s2 + z) != '\0')
 	{
@@ -49,7 +39,6 @@ char *str_concat(char *s1, char *s2)
 		z++;
 		count++;
 	}
-
 	concat[count + 1] = '\0';
 	return (concat);
 }
