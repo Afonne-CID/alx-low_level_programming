a797047ee24712f67650b7e95ce5700fe92d5c0e
Afonne-CID <delightinbusiness@gmail.com>
2021-07-14 15:27:39 -0700

    done

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,15 +13,6 @@ void free_grid(int **grid, int height)
 {
 	int i = 0;
 
-	if (grid[i] == NULL)
-	{
-		for (i = i - 1; i >= 0; i--)
-		{
-			free(grid[i]);
-		}
-		free(grid);
-	}
-
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
