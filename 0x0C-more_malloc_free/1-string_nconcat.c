80c979c975f5ec828c2b4209d26be3974c6ffdab
Afonne-CID <delightinbusiness@gmail.com>
2021-07-15 11:20:49 -0700

    newest commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,62 @@
-#include <stdlib.h>
 #include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strlen - give the length of a string
+ * @s: the string
+ *
+ * Return: the length of a string
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+	return (i);
+}
 
 /**
  * string_nconcat - concatenates two strings
  * @s1: string 1
  * @s2: string 2
- * @n: number of bytes to copy
+ * @n: bytes of s2
  *
- * Return: result
+ * Return: a pointer with the content of s1 followed by n byte of s2
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	int i, j, z, count;
+	unsigned int length1, length2, i, j;
+	char *space;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-	for (i = 0; *(s1 + i) != '\0';)
-		i++;
-	for (j = 0; *(s2 + j) != '\0'; j++)
-	{
-		i++;
 	}
-	i = i + 2;
-	concat = malloc(sizeof(char*) * i);
-	if (concat == NULL)
+
+	length1 = _strlen(s1);
+	length2 = _strlen(s2);
+
+	if (n >= length2)
+	{
+		n = length2;
+	}
+
+	space = malloc(sizeof(char) * (n + length1 + 1));
+
+	if (space == NULL)
+	{
 		return (NULL);
-	count = 0;
-	while (*(s1 + count) != '\0')
-	{
-		concat[count] = *(s1 + count);
-		count++;
 	}
-	z = 0;
-	while (*(s2 + z) != '\0' && z < (int) n)
-	{
-		concat[count] = *(s2 + z);
-		z++;
-		count++;
-	}
-	concat[count + 1] = '\0';
-	return (concat);
+
+	for (i = 0 ; i < length1 ; i++)
+		space[i] = s1[i];
+	for (j = 0 ; s2[j] != '\0' && j != n ; j++, i++)
+		space[i] = s2[j];
+
+	space[i] = '\0';
+	return (space);
 }
