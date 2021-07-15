dbc4d8c949cf524aa1082cf1c4465819163f9c08
Afonne-CID <delightinbusiness@gmail.com>
2021-07-15 12:29:35 -0700

    on top commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	unsigned int len = n, index;
+	unsigned int i, j, count;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,24 +22,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (index = 0; s1[index]; index++)
-	{
-		len++;
-	}
-
-	concat = malloc(sizeof(char) * len);
-	if (concat == NULL)
-		return (NULL);
-
-	len = 0;
-
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
+        for (i = 0; *(s1 + i) != '\0';)
+                i++;
+        for (j = 0; *(s2 + j) != '\0'; j++)
+                i++;
+        i++;
+        concat = malloc(sizeof(char) * i);
+        if (concat == NULL)
+                return (NULL);
+        count = 0;
+        while (*(s1 + count) != '\0')
+        {
+                concat[count] = *(s1 + count);
+                count++;
+        }
+        z = 0;
+        while (*(s2 + z) != '\0' && z < n)
+        {
+                concat[count] = *(s2 + z);
+                z++;
+                count++;
+        }
+        concat[count + 1] = '\0';
 
 	return (concat);
 }
