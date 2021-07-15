ed25381e0708ff19803a067b8f9c70dc8ec206c0
Afonne-CID <delightinbusiness@gmail.com>
2021-07-15 13:45:51 -0700

    on top commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	grid = malloc(sizeof(size * nmemb));
+	grid = malloc(size * nmemb);
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
