619f06838e5432faa238808aa66c178e29f2579f
Afonne-CID <delightinbusiness@gmail.com>
2021-07-15 11:59:21 -0700

    on top commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len++;
 
-	concat = malloc(sizeof(char) * (len));
+	concat = malloc(sizeof(char) * (len) + 1);
 	if (concat == NULL)
 		return (NULL);
 
