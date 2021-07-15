5183730af8b337a5ce881037aec2ccbd9c76e4bc
Afonne-CID <delightinbusiness@gmail.com>
2021-07-15 11:16:21 -0700

    newest commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * mul - mul is a program that multiplies two positive numbers
+ * @num1: first param
+ * @num2: second param
+ *
+ * Return: product of two numbers
+ */
+int mul(int **num1, int **num2)
+{
+	
+}
