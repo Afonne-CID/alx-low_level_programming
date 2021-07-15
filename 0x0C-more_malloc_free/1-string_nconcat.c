99a8cf383e0cf55b80d20259dbda9cc1e2c4dd42
Afonne-CID <delightinbusiness@gmail.com>
2021-07-15 10:53:51 -0700

    edit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,9 +21,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; *(s1 + i) != '\0';)
 		i++;
 	for (j = 0; *(s2 + j) != '\0'; j++)
+	{
 		i++;
-	i++;
-	concat = malloc(sizeof(char) * i + 1);
+	}
+	concat = malloc(sizeof(char) * i);
 	if (concat == NULL)
 		return (NULL);
 	count = 0;
