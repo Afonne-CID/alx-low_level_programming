0528e413aebb07eef039541154431b269f989fd2
Afonne-CID <delightinbusiness@gmail.com>
2021-07-15 05:57:19 -0700

    committed

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: paramater 1- the beginning of the range
+ * @max: the max of the range
+ *
+ * Return: result
+ */
+int *array_range(int min, int max)
+{
+	int i, j, z = 0;
+	int *grid;
+
+	if (min > max)
+		return (NULL);
+	for (j = min; j < max; j++)
+	{
+		i++;
+	}
+	i++;
+	grid = malloc(sizeof(int) * i);
+	if (grid == NULL)
+		return (NULL);
+	for (i = min; i < max + 1; i++)
+	{
+		grid[z] = i;
+		z++;
+	}
+	return (grid);
+}
