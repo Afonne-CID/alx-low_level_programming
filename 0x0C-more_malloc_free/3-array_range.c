6c7614a2fc1f4ad67434c75c9e2ff8ca7e67742c
Afonne-CID <delightinbusiness@gmail.com>
2021-07-17 01:42:47 -0700

    on top commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,23 +11,18 @@
  */
 int *array_range(int min, int max)
 {
-	int i, j, z = 0;
-        int *grid;
+	int i;
+	int *grid;
 
 	if (min > max)
 		return (NULL);
-	for (j = min; j < max; j++)
-	{
-		i++;
-	}
-	i++;
-	grid = malloc(sizeof(int) * i);
+	grid = malloc(sizeof(int) * (max - min + 1));
 	if (grid == NULL)
 		return (NULL);
-	for (i = min; i < max + 1; i++)
+	for (i = 0; min <= max; i++)
 	{
-		grid[z] = i;
-		z++;
+		grid[i] = min;
+		min++;
 	}
 	return (grid);
 }
