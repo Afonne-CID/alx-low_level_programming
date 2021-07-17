ae623f82eae28d37c899611cad46720fc5957054
Afonne-CID <delightinbusiness@gmail.com>
2021-07-17 09:42:58 -0700

    auto commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
 	{
-		*((char*)grid + i) = 0;
+		*((char *)grid + i) = 0;
 	}
 	return (grid);
 }
