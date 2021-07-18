41a1b4d883a2d98cec51f81fa82a28aa0f87f302
Afonne-CID <delightinbusiness@gmail.com>
2021-07-18 10:34:28 -0700

    auto commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,56 +15,41 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	tmp = ptr;
-
+	
 	if (new_size == old_size)
 	{
 		return (ptr);
 	}
 
-	if (ptr != NULL && new_size == 0)
-	{
-		free(ptr);
-		return (ptr);
-	}
-
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
-			return (ptr);
-	}
-
-	if (new_size < old_size)
+	if (ptr == NULL && new_size != 0)
 	{
 		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-		{
-			free(new_ptr);
-			return (NULL);
-		}
-                i = 0;
-                while (i < new_size)
-                {
-			new_ptr[i] = tmp[i];
-			i++;
-                }
-	}
-
-	if (new_size > old_size)
-	{
-		new_ptr = malloc((new_size - old_size) + old_size);
 		if (new_ptr == NULL)
 		{
 			free(ptr);
 			return (NULL);
 		}
-		
-		i = 0;
-		while (i < old_size && i < new_size)
-		{
-			new_ptr[i] = tmp[i];
-			i++;
-		}
+		free(ptr);
+		return (new_ptr);
 	}
-
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (ptr);
+	}
+	new_ptr = malloc(old_size + (new_size - old_size));
+	if (new_ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+       
+	i = 0;
+	while (i < old_size && i < new_size)
+	{
+		new_ptr[i] = tmp[i];
+		i++;
+        }
+	free(ptr);
 	return (new_ptr);
 }
