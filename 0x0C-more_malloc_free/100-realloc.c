b18bd9a49e1a8e1e0baf594bc34a7ee01cf4f9ca
Afonne-CID <delightinbusiness@gmail.com>
2021-07-18 06:53:51 -0700

    auto commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		}
 		if (new_size > old_size)
 		{
-			new_ptr = malloc((new_size - old_size) + old_size);
+			new_ptr = malloc(old_size + (new_size - old_size));
 
 			i = 0;
 			while (*((char *)ptr + i))
