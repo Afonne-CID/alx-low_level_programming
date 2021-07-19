7ad4e473ad1cd7683cac16176b54ffe281834444
Afonne-CID <delightinbusiness@gmail.com>
2021-07-19 12:55:18 -0700

    auto commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,14 @@
  *
  * This struct is to be populated with the details specified above
  */
-typedef struct dog dog_t
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
