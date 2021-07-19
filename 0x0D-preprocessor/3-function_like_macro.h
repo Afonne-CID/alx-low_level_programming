0b96d6063fdbfcb7a249b0984075684563860270
Afonne-CID <delightinbusiness@gmail.com>
2021-07-19 09:31:03 -0700

    fixed and tested

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTIONS_LIKE_MACRO_H
 #define FUNCTIONS_LIKE_MACRO_H
 
-#define ABS(x) ((x) * (-1))
+#define ABS(x) x < 0 ? ((x) * (-1)) : x
 
 #endif /*ifndef FUNCTIONS_LIKE_MACRO_H ends here*/
