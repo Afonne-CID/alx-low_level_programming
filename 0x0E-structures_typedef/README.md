dfa0ee0516ad4f5896d62d4d6fbad41a045e7f68
Afonne-CID <delightinbusiness@gmail.com>
2021-07-19 10:48:22 -0700

    initial commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This project is a little intro to what structures are, when, why and how to use them. Also, we take a look at typedef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - Contains Bio Data of my pet dog
+ * @name: Name of my little dog
+ * @age: Dog's age
+ * @owner: The owner
+ *
+ * This struct is to be populated with the details specified above
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*ifndef dog_h ends here*/
