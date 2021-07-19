3a60dd7f743cdc241c40b5cefe48282e7ac080e4
Afonne-CID <delightinbusiness@gmail.com>
2021-07-19 13:13:13 -0700

    auto commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ *
+ * Return: returns struct type
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *billy;
+
+	billy = malloc(sizeof(dog_t));
+
+	billy->name = name;
+	billy->age = age;
+	billy->owner = owner;
+
+	return (billy);
+}
