8170fe0684a3059ececcb6e96b15a82af5abe841
Afonne-CID <delightinbusiness@gmail.com>
2021-07-19 21:18:46 -0700

    auto commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,3 +23,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (billy);
 }
+
+/**
+ * main - storing a copy of name and owner
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	dog_t *billy;
+
+	billy = new_dog("Billy", 10, "Paul");
+	if (billy == NULL)
+		return (1);
+	return (0);
+}
