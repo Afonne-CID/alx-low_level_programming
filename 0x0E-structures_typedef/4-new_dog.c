6509a8c66acae3d402b813f76aa581033ab28f62
Afonne-CID <delightinbusiness@gmail.com>
2021-07-19 23:42:15 -0700

    auto commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,17 +33,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	i = 0;
-	while (src[i] != '\0')
+	for (i = 0; src[i]; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
-	i++;
 	dest[i] = '\0';
 	return (dest);
 }
-
 
 /**
  * new_dog - creates a new dog
