9537e2095185009bcf8c3294b8feb27ec6d84757
Afonne-CID <delightinbusiness@gmail.com>
2021-07-20 10:41:05 -0700

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name string to be printed
+ * @f: function call back
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		(*f)(name);
+}
