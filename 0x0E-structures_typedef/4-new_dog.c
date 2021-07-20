38a407b0905466003691069e9a878b1566b02860
Afonne-CID <delightinbusiness@gmail.com>
2021-07-19 23:34:30 -0700

    auto commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,48 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
+
+int _strlen(char *c);
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _strlen - calculates length of string
+ * @c: string to be calculated
+ *
+ * Return: String length
+ */
+int _strlen(char *c)
+{
+	int i;
+
+	i = 0;
+	while (*c++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copies a string from a source to another destination
+ * @dest: destination to be copied to
+ * @src: source from to copy
+ *
+ * Return: the address of the destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
 
 /**
  * new_dog - creates a new dog
@@ -20,10 +62,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(billy);
 		return (NULL);
 	}
+	billy->name = malloc(sizeof(char) * ((_strlen(name)) + 1));
+	if (billy->name == NULL)
+	{
+		free(billy);
+		return (NULL);
+	}
+	billy->name = _strcpy(billy->name, name);
 
-	billy->name = name;
 	billy->age = age;
-	billy->owner = owner;
+
+	billy->owner = malloc(sizeof(char) * ((_strlen(owner)) + 1));
+	if (billy->owner == NULL)
+	{
+		free(billy);
+		return (NULL);
+	}
+	billy->owner = _strcpy(billy->name, owner);
 
 	return (billy);
 }
