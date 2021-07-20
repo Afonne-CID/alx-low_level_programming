f30307770944a50dc0a5ee14b5f6609ca7e4255d
Afonne-CID <delightinbusiness@gmail.com>
2021-07-20 00:21:20 -0700

    auto commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,9 +53,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *billy;
 
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-
 	billy = malloc(sizeof(dog_t));
 	if (billy == NULL)
 		return (NULL);
