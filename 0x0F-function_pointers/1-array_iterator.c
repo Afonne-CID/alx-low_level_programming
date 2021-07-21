7bc0d5faeaacf4fc1310ead96b96a1dc6a5b1af5
Afonne-CID <delightinbusiness@gmail.com>
2021-07-20 20:25:50 -0700

    function by pointer iterates on elem of array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function iterates through an array
+ * @array: array to be iterated on
+ * @size: size of array
+ * @action: pointer to function (array_iterator);
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || size == 0)
+		return;
+
+	i = 0;
+	while (size-- > 0)
+	{
+		action(array[i++]);
+	}
+}
