5ba2b176752d6399751d7078f2939b8394ec69ac
Afonne-CID <delightinbusiness@gmail.com>
2021-07-23 07:08:12 -0700

    error fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,15 +4,15 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - function that prints numbers, followed by a new line
- * @separator: the string to be printed betwen numbers
+ * print_strings - function that prints numbers, followed by a new line
+ * @separator: the string to be printed betwen strings
  * @n: number of integers passed to 'print_numbers'
  *
  * Return: result
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
- 	unsigned int i;
+	unsigned int i;
 	char *s;
 
 	va_list ap;
@@ -24,12 +24,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		s = va_arg(ap, char *);
 
 		if (s == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", s);
-		
+
 		if (separator != NULL && i != n - 1)
-		{	
+		{
 			printf("%s", separator);
 		}
 	}
