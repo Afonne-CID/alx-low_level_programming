9b195cc7ea5faee7986f55e0265d0b8e55b5f89d
Afonne-CID <delightinbusiness@gmail.com>
2021-08-12 05:45:10 -0700

    header

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -40,3 +45,5 @@ char *leet(char *);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
+
+#endif
