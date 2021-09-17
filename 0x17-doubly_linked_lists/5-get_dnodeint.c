a783fa6adc4f470894eee48570402593d04d415b
Afonne-CID <delightinbusiness@gmail.com>
2021-09-16 17:26:44 -0700

    get node at

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (cnt != index)
 	{
 		cnt++;
-		if (head == NULL)
+		if (tmp == NULL)
 			return (NULL);
 		tmp = tmp->next;
 	}
