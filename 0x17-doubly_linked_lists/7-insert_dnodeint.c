d3467eb479d4c4d08a126045faff86da87251436
Afonne-CID <delightinbusiness@gmail.com>
2021-09-16 18:48:37 -0700

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,6 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *tmp = *h;
-	unsigned int cnt = 0;
 
 	if (*h == NULL)
 		return (NULL);
@@ -23,13 +22,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (cnt != (idx - 1))
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+/*	while (cnt != (idx - 1))
 	{
 		cnt++;
 		if (!tmp)
 			return (NULL);
 		tmp = tmp->next;
-	}
+	}*/
 	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
