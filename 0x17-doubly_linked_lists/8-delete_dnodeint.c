e3dcb1bf4f40634589696cf29a54378b4bfc2191
Afonne-CID <delightinbusiness@gmail.com>
2021-12-09 21:58:59 -0800

    explicit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -25,7 +25,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (tmp == *head)
 	{
 		*head = tmp->next;
-		if (*head)
+		if (*head != NULL)
 			(*head)->prev = NULL;
 		return (1);
 	}
